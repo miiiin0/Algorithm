fab44e91734b0e9f07a74e9cdb392ff591d32a03
miiiin0 <79246888+miiiin0@users.noreply.github.com>
2023-04-03 17:49:40 +0900

    Create boj_1978.java

---
+++ BaekJoon_java/boj_1978.java
@@ -0,0 +1,28 @@
+package BaekJoon_java;
+
+import java.util.*;
+import java.io.*;
+
+public class boj_1978 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int caseNum = Integer.parseInt(br.readLine());
+        int num;
+        int result=0;
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for(int i=0; i<caseNum; i++) {
+            num = Integer.parseInt(st.nextToken());
+            for(int j=2; j<=num; j++) {
+                if(j==num) {
+                    result++;
+                }
+                else if (num%j == 0) {
+                    break;
+                }
+
+            }
+        }
+        System.out.println(result);
+    }
+}
